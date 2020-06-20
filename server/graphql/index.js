ab77f16ac530bf177153002fd054371de5605217
Chris Miller <chris@paro.io>
2020-06-20 12:51:46 -0400

    graphql

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,15 +1,12 @@
 const { ApolloServer, gql } = require('apollo-server-express')
 const { User } = require('../db/models')
 
-const router = require('express').Router()
-module.exports = router
-
 const typeDefs = gql`
   type Query {
     getUser(id: Int!): User
     getUsers: [User]
-    getArtist(id: Int!): Artist
-    getArtists: [Artist]
+    # getArtist(id: Int!): Artist
+    # getArtists: [Artist]
   }
 
   type User {
@@ -18,25 +15,29 @@ const typeDefs = gql`
     name: String!
   }
 
-  type Artist {
-    id: Int!
-    name: String!
-    spotifyId: String!
-    followers: Int!
-    popularity: Int!
-    genres: [String!]
-    userId: Int!
-    imageUrl: String!
-  }
+  #   type Artist {
+  #     id: Int!
+  #     name: String!
+  #     spotifyId: String!
+  #     followers: Int!
+  #     popularity: Int!
+  #     genres: [String!]
+  #     userId: Int!
+  #     imageUrl: String!
+  #   }
 `
 
 const resolvers = {
   Query: {
     getUser: (parent, { id }) => User.findByPk(id),
-    getUsers: () => User.findAll(),
-    getArtist: (parent, { id }) => Artist.findByPk(id),
-    getArtists: () => Artist.findAll()
+    getUsers: () => User.findAll()
+    // getArtist: (parent, { id }) => Artist.
   }
 }
 
-module.exports = new ApolloServer({ typeDefs, resolvers })
+const context = expressContext => {
+  const { req, res } = expressContext
+  console.log(req, res)
+}
+
+module.exports = new ApolloServer({ typeDefs, resolvers, context })
